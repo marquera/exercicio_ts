c48cf8b306d158d8d019a4d98a46f9534f384974
Marcos Vinicius <marcos.vinicius.correia2011@gmail.com>
2024-03-19 22:03:50 -0300

    first commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(5, 3));
+console.log(saudacao('João'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,13 @@
+
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+console.log(multiplicacao(5, 3));
+console.log(saudacao('João')); 
